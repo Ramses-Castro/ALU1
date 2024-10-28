b470285531e59d6cc538f255be779bab0581851f
Ramses-Castro <vcl06@hotmail.com>
2024-10-28 17:31:51 -0600

    project.v

--- src/project.v
+++ src/project.v
@@ -5,15 +5,15 @@
 
 `default_nettype none
 
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // will go high when the design is enabled
+module ALU_Displays (
+    input  wire [2:0] ui_in,  //in1
+    output wire [3:0] uo_out, // select disp
+    input  wire [2:0] uio_in, // in2
+    output wire [6:0] uio_out,  // segmentos (AE-GE)
+  //  output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena, //error     // will go high when the design is enabled
     input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+   // input  wire       rst_n     // reset_n - low to reset
 );
 
     // Señales internas
