081729d25bc09e4d57ff4ce51fc2d32d801c198d
Ramses-Castro <vcl06@hotmail.com>
2024-11-08 03:44:52 -0700

    Multiplexor2_1.v

--- src/Multiplexor2_1.v
+++ src/Multiplexor2_1.v
@@ -1,12 +1,10 @@
 `default_nettype none
 
-module Multiplexor2_1 (
-    input wire [3:0] J,     // Entrada J de 4 bits
-    input wire [3:0] K,     // Entrada K de 4 bits
-    input wire EL,          // Señal de control (selector)
-    output wire [3:0] SA    // Salida de 4 bits
+module Multiplexor2_1(
+    input wire A,        // Entrada A
+    input wire B,        // Entrada B
+    input wire sel,      // Selector (0 o 1)
+    output wire out      // Salida
 );
-
-assign SA = (EL == 1'b0) ? J : K;  // Selección de la salida con base en EL
-
+    assign out = (sel) ? B : A; // Si sel es 1, salida es B, si sel es 0, salida es A
 endmodule
